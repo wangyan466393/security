014cc6ddbcd5205b48915916f694ef80cc1e739b
wangyan <251140742@qq.com>
2020-07-20 17:55:04 +0800

    验证token是否过期

--- src/ajax.js
+++ src/ajax.js
@@ -1,9 +1,31 @@
+
 import axios from 'axios';
+import ajax from './router';
 // axios发送ajax请求
-// 1.直接使用axios函数
-// 2.借助辅助函数
-// 3.创建实例对象
-const request = axios.create({
+const _axios = axios.create({
     baseURL:'http://172.16.1.104:8000'
 })
-export default request;
+// 配置默认的host,假如你的API host是：http://api.htmlx.club
+// 添加请求拦截器
+_axios.interceptors.request.use(function (config) {
+    　　// 在发送请求之前做些什么
+    if (window.localStorage.getItem('userToken')) {
+        config.headers.Authorization = window.localStorage.getItem('userToken');
+    }
+       console.log(window.localStorage.getItem('userToken'));
+    　　return config
+    }, function (error) {
+    　　// 对请求错误做些什么
+       console.log(ajax+'   1');
+      return Promise.reject(error)
+    });    
+    // 添加响应拦截器
+_axios.interceptors.response.use(function (response) {
+    　　// 对响应数据做点什么
+    　　return response
+    }, function (error) {
+    　　// 对响应错误做点什么
+        window.location.href='/';
+    　　return Promise.reject(error)
+    })  
+export default _axios;
